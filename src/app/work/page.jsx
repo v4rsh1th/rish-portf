253204e0e1c88b7987942d0bdd49993b6bdbe90f
v4rsh1th <v4rsh1th@gmail.com>
2024-10-25 15:55:04 -0400

    add favicon

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -6,7 +6,7 @@ export default function Work() {
         <div className={styles.container}>
             <Header />
             <div className={styles.workContent}>
-                <h1>My Work</h1>
+                <h1>Work</h1>
                 <p>Gotta put all the work you did in here.</p>
             </div>
         </div>
