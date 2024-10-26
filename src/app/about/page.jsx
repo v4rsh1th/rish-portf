030f807e6f062077ce2b2309261056e726076903
v4rsh1th <v4rsh1th@gmail.com>
2024-10-26 07:41:48 -0400

    Fix formatting

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,14 +2,14 @@ import Header from '../components/Header';
 import styles from '../page.module.css';
 
 export default function About() {
-    return (
-        <div className={styles.container}>
-            <Header />
-            <div className={styles.aboutContent}>
-                <h1 className={styles.h1}>About</h1>
-                <p>Hi! I am a 24 year old designer from Arizona with experience in marketing and social media management for over 3 years with a Master's degree in Engineering and Industrial Management.</p> <br />
-                <p>I have a passion for design, art, management and lifestyle and a keen eye for capturing and creating compelling and aesthetically pleasing high quality content that is authentic and connects with your target audience to boost engagement and drive conversions.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.aboutContent}>
+        <h1 className={styles.h1}>About</h1>
+        <p>Hi! I am a 24 year old designer from Arizona with experience in marketing and social media management for over 3 years with a Master's degree in Engineering and Industrial Management.</p> <br />
+        <p>I have a passion for design, art, management and lifestyle and a keen eye for capturing and creating compelling and aesthetically pleasing high quality content that is authentic and connects with your target audience to boost engagement and drive conversions.</p>
+      </div>
+    </div>
+  );
 }
--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -2,13 +2,13 @@ import Header from '../components/Header';
 import styles from '../page.module.css';
 
 export default function Work() {
-    return (
-        <div className={styles.container}>
-            <Header />
-            <div className={styles.workContent}>
-                <h1>Work</h1>
-                <p>Gotta put all the work you did in here.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.workContent}>
+        <h1>Work</h1>
+        <p>Gotta put all the work you did in here.</p>
+      </div>
+    </div>
+  );
 }
