b603df98351940d1ddf6920648ae6f5a312c18f3
v4rsh1th <v4rsh1th@gmail.com>
2024-10-28 16:43:29 -0400

    Add resume

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import ResumeButton from '../components/ResumeButton';
 import styles from '../page.module.css';
 
 export default function About() {
@@ -9,6 +10,13 @@ export default function About() {
         <h1 className={styles.h1}>About</h1>
         <p>Hi! I am a 24 year old designer from Arizona with experience in marketing and social media management for over 3 years with a Master's degree in Engineering and Industrial Management.</p> <br />
         <p>I have a passion for design, art, management and lifestyle and a keen eye for capturing and creating compelling and aesthetically pleasing high quality content that is authentic and connects with your target audience to boost engagement and drive conversions.</p>
+        <br />
+        <span>
+          <p>
+            Here's my resume: &nbsp;
+            <ResumeButton />
+          </p>
+        </span>
       </div>
     </div>
   );
---
+++ src/app/components/ResumeButton.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css';
+
+export default function ResumeButton() {
+  return (
+    <a href="/files/resume.pdf" target="_blank" rel="noopener noreferrer" className={styles.button74} >
+      View Resume
+    </a>
+  );
+}
